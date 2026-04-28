854d2b885d12f201fe32f0ec5d5f7adc56897cf6
røsus <190874852+rosuz@users.noreply.github.com>
2026-04-28 16:07:42 +0100

    refactor: consolidate theme symlinks to install phase
    - Move btop/mako/background symlink creation from apply-theme to
    links.sh

--- install/config/links.sh
+++ install/config/links.sh
@@ -18,4 +18,20 @@ for item in "$DOTFILES_PATH/config"/*; do
   fi
 done
 
+info "Creating theme symlinks..."
+
+mkdir -p ~/.config/btop/themes
+ln -sf "$DOTFILES_PATH/themes/current/btop.theme" ~/.config/btop/themes/current.theme
+
+mkdir -p ~/.config/mako
+ln -sf "$DOTFILES_PATH/themes/current/mako.ini" ~/.config/mako/config
+
+if [[ -d "$DOTFILES_PATH/themes/current/backgrounds" ]]; then
+  first_bg=$(ls "$DOTFILES_PATH/themes/current/backgrounds"/* 2>/dev/null | head -1)
+  if [[ -n "$first_bg" ]]; then
+    ln -nsf "backgrounds/$(basename "$first_bg")" "$DOTFILES_PATH/themes/current/background"
+  fi
+fi
+
 success "Config files copied!"
+success "Theme symlinks created!"
